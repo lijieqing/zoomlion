ba98c909cde4a7af4cdd1c08511a48baae35cf68
master <759353118@qq.com>
2017-08-02 17:26:03 +0800

    项目记录展示view header 实现

--- app/src/main/java/com/kstech/zoomlion/MainActivity.java
+++ app/src/main/java/com/kstech/zoomlion/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -22,6 +23,7 @@ import com.kstech.zoomlion.utils.JsonUtils;
 import com.kstech.zoomlion.utils.LogUtils;
 import com.kstech.zoomlion.utils.MyHttpUtils;
 import com.kstech.zoomlion.view.ItemShowView;
+import com.kstech.zoomlion.view.adapter.ExpandItemAdapter;
 import com.kstech.zoomlion.view.adapter.ItemAdapter;
 
 import java.io.IOException;
@@ -30,15 +32,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity{
     private TextView tvNet;
     ItemShowView itemShowView;
-    ListView lvItem;
+    ExpandableListView elvItem;
     private List<String> oneItem = new ArrayList<>();
     public DeviceModelFile modelFile;
     public List<CheckItemVO> itemList = new ArrayList<>();
-    ItemAdapter itemAdapter;
+    ExpandItemAdapter expandItemAdapter;
+    List<String> groups = new ArrayList<>();
+    Map<String,List<CheckItemVO>> checkItemMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +52,14 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         tvNet = (TextView) findViewById(R.id.tv_net);
         itemShowView = (ItemShowView) findViewById(R.id.isv);
-        lvItem = (ListView) findViewById(R.id.lv_item);
-        itemAdapter = new ItemAdapter(itemList,this);
-        lvItem.setAdapter(itemAdapter);
-        lvItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        elvItem = (ExpandableListView) findViewById(R.id.elv_item);
+        expandItemAdapter = new ExpandItemAdapter(this,groups,checkItemMap);
+        elvItem.setAdapter(expandItemAdapter);
+        elvItem.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                CheckItemVO item = modelFile.getCheckItemList().get(i);
+            public boolean onChildClick(ExpandableListView expandableListView, View view, int i, int i1, long l) {
+                String key = groups.get(i);
+                CheckItemVO item = checkItemMap.get(key).get(i1);
                 itemShowView.updateHead(item);
                 //// TODO: 2017/8/2 查询数据库更新body 调用itemShowView.updateBody();,下面使用伪数据
                 List<CheckItemParamValueVO> volist = item.getParamNameList();
@@ -68,6 +75,7 @@ public class MainActivity extends AppCompatActivity{
                 }
 
                 itemShowView.updateBody(details);
+                return false;
             }
         });
         new Thread(){
@@ -141,40 +149,6 @@ public class MainActivity extends AppCompatActivity{
         dialog.show();
     }
 
-    private void initData(){
-        Globals.paramHeadVOs.clear();
-        String source0 = "{\"itemName\":\"压力检测\",\"paramName\":\"底盘\",\"value\":\"40\",\"unit\":\"\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-        String source1 = "{\"itemName\":\"压力检测\",\"paramName\":\"底盘\",\"value\":\"40\",\"unit\":\"\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-        String source2 = "{\"itemName\":\"压力检测\",\"paramName\":\"底盘\",\"value\":\"40\",\"unit\":\"\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-        String source3 = "{\"itemName\":\"压力检测\",\"paramName\":\"灯管\",\"value\":\"40\",\"unit\":\"\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-        String source4 = "{\"itemName\":\"压力检测\",\"paramName\":\"油缸沉降量(洞壁深处)\",\"value\":\"40\",\"unit\":\"℃\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-        String source5 = "{\"itemName\":\"压力检测\",\"paramName\":\"左回转压力\",\"value\":\"40\",\"unit\":\"℃\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-        String source6 = "{\"itemName\":\"压力检测\",\"paramName\":\"左回转压力\",\"value\":\"40\",\"unit\":\"℃\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-        String source7 = "{\"itemName\":\"压力检测\",\"paramName\":\"左回转压力\",\"value\":\"40\",\"unit\":\"℃\",\"type\":\"环境参数\",\"validMin\":\"30\",\"validMax\":\"50\",\"validAvg\":\"40\",\"imgIds\":\"\",\"chartIds\":\"\"}";
-
-        CheckItemParamValueVO checkItemParamValueVO;
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source0, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source1, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source2, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source3, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source4, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source5, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source6, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-        checkItemParamValueVO = (CheckItemParamValueVO) JsonUtils.fromJson(source7, CheckItemParamValueVO.class);
-        Globals.paramHeadVOs.add(checkItemParamValueVO);
-
-        String s = JsonUtils.toJson(Globals.paramHeadVOs);
-        oneItem.add(s);
-        LogUtils.e("ItemShowView",s);
-    }
-
     public void update(View view) {
 
     }
@@ -193,7 +167,10 @@ public class MainActivity extends AppCompatActivity{
             MainActivity mActivity = mainActivity.get();
             if (mActivity != null){
                 mActivity.itemList.addAll(mActivity.modelFile.getCheckItemList());
-                mActivity.itemAdapter.notifyDataSetChanged();
+                Set<String> keys = mActivity.modelFile.checkItemMap.keySet();
+                mActivity.groups.addAll(keys);
+                mActivity.checkItemMap.putAll(mActivity.modelFile.checkItemMap);
+                mActivity.expandItemAdapter.notifyDataSetChanged();
                 mActivity.itemShowView.updateHead(mActivity.modelFile.getCheckItemList().get(0));
             }
         }
