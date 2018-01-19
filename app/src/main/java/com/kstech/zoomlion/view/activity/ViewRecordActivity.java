cfee16d1b445184f513231c5cb049f6749941f36
lijieqing <759353118@qq.com>
2018-01-19 14:50:27 +0800

    新增调试记录查看界面

--- app/src/main/java/com/kstech/zoomlion/view/activity/ViewRecordActivity.java
+++ app/src/main/java/com/kstech/zoomlion/view/activity/ViewRecordActivity.java
@@ -1,18 +1,29 @@
 package com.kstech.zoomlion.view.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.kstech.zoomlion.R;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
 
 @ContentView(R.layout.activity_view_record)
 public class ViewRecordActivity extends BaseActivity {
 
+    @ViewInject(R.id.vr_rv)
+    private RecyclerView rvItemRecord;
+
+    @ViewInject(R.id.vr_tv_title)
+    private TextView tvTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         x.view().inject(this);
+        tvTitle.setText("此界面用于二次调试时的记录查看");
+        tvTitle.setTextSize(18);
     }
 }
